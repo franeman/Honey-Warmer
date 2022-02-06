d59e56093eb9fe42cee2a3db13216a29cb70b7f5
Franeman <franeman@yahoo.com>
2022-02-06 11:02:42 -0500

    Added self to hysteresis variables

--- honeyWarmer.py
+++ honeyWarmer.py
@@ -33,17 +33,17 @@ class Hysteresis: # A class that defines a boolean value with hysteresis
     def __init__(self, setPoint1, setPoint2, inverted=0):
         self.setPoint1 = setPoint1 # The value needed to be passed in order to initially change the result
         self.setPoint2 = setPoint2 # The value needed to fall below in order to change the result back
-        result = inverted
-        passedPoint1 = 0
+        self.result = inverted
+        self.passedPoint1 = 0
     
     def testVal(self, val):
-        if (val >= setPoint1 and not passedPoint1): # Value is passing the first set point
-            result = not result
-            passedPoint1 = 1
-        elif (val <= setPoint2 and passedPoint1): # Value fell below the second set point
-            result = not result
-            passedPoint1 = 0
-        return result
+        if (val >= self.setPoint1 and not self.passedPoint1): # Value is passing the first set point
+            self.result = not self.result
+            self.passedPoint1 = 1
+        elif (val <= self.setPoint2 and self.passedPoint1): # Value fell below the second set point
+            self.result = not self.result
+            self.passedPoint1 = 0
+        return self.result
 
 # Setup MQTT
 mqtt = paho.mqtt.client.Client() # Create a MQTT client object named mqtt
@@ -53,13 +53,13 @@ mqtt.publish("/debug", "Honey warmer connected!") # Publish connected message to
 
 # Configure GPIO
 GPIO.setmode(GPIO.BCM) # Use GPIO numbering scheme (use GPIO.BOARD for physical pin numbering)
-plate1 = 2 # Plate 1 is GPIO2 (physical pin 3)
-plate2 = 3 # Plate 2 is GPIO3 (physical pin 5)
+plate1 = 6 # Plate 1 is GPIO6 (physical pin 31)
+plate2 = 26 # Plate 2 is GPIO26 (physical pin 37)
 GPIO.setup(plate1, GPIO.OUT, initial=GPIO.LOW) # Set plate1 as an output
 GPIO.setup(plate2, GPIO.OUT, initial=GPIO.LOW) # Set plate2 as an output
 
 # Configure DHT11
-dataPin = 26 # DHT11 data pin is connected to GPIO19 (physical pin 37)
+dataPin = 19 # DHT11 data pin is connected to GPIO19 (physical pin 37)
 dht11 = dht11.DHT11(dataPin) # Create an instance of the DHT11 class called dht11 who's signal pin is dataPin
 
 targetTemp = 105 # Target temp to keep the honey at in F
@@ -73,10 +73,10 @@ runOnePlate = Hysteresis(targetTemp, targetTemp-tolerance, 1)
 while (True): # Run forever
     result = readDHT(dht11, mqtt) # Read the temp and humidity, publish reading to MQTT
     temp = convertCToF(result.temperature) # Calculate the temperature in degrees F
-    if (runTwoPlates.testVal(temp): # If the temp needs to be raised quickly, run both heaters
+    if (runTwoPlates.testVal(temp)): # If the temp needs to be raised quickly, run both heaters
         GPIO.output(plate1, GPIO.HIGH)
         GPIO.output(plate2, GPIO.HIGH)
-    elif (runOnePlate.testVal(temp): # If the temp is close, run 1 heater 
+    elif (runOnePlate.testVal(temp)): # If the temp is close, run 1 heater 
         GPIO.output(plate1, GPIO.HIGH)
         GPIO.output(plate2, GPIO.LOW)
     else: # Temp is in tolerance, turn off the heaters
